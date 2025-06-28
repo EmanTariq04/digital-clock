a763cdc59a0f75432342515656a6f2c0db5d83cb
Eman Tariq <emantariq.1004@gmail.com>
2025-06-28 18:30:11 +0500

    final commit

--- script.js
+++ script.js
@@ -7,17 +7,22 @@
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
-    let ampm = h >= 12 ? "PM" : "AM";
+    let ampm = "AM";
+    // let ampm = h >= 12 ? "PM" : "AM";
 
-    // if (h > 12) {
-    //     h = h - 12
-    //     ampm = "PM";
-    // }
+    if (h > 12) {
+        h = h - 12
+        ampm = "PM";
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
 
     hourEl.innerText = h;
     minuteEl.innerText = m;
     secondEl.innerText = s;
-    ampmEl, (innerText = ampm);
+    ampmEl.innerText = ampm;
     setTimeout(() => {
         updateClock();
     }, 1000);
